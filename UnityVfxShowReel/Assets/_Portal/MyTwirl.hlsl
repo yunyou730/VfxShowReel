96d3dfe05f9a5f14f9c987ab88036ad07d428968
yunyou <502765299@qq.com>
2024-12-22 19:44:07 +0800

    +尝试控制了 twirl 的 角度 计算方式, 如果加一个负号 可以反过来旋转

--- UnityVfxShowReel/Assets/_Portal/MyTwirl.hlsl
+++ UnityVfxShowReel/Assets/_Portal/MyTwirl.hlsl
@@ -5,6 +5,7 @@ void AyyTwirl_float(float2 UV, float2 Center, float Strength, float2 Offset, out
 {
     float2 delta = UV - Center;
     float angle = Strength * length(delta);
+    //angle *= -1;   // rotate direction
     float x = cos(angle) * delta.x - sin(angle) * delta.y;
     float y = sin(angle) * delta.x + cos(angle) * delta.y;
     Out = float2(x + Center.x + Offset.x, y + Center.y + Offset.y);
