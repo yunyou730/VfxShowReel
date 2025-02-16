711562bbf7c1ff69aed2d5fc20dbbd4b8442ec3f
苗云龙 <miaoyunlong@miaoyunlongs-MacBook-Pro.local>
2025-02-16 15:08:52 +0800

    RenderGraph 的 基础准备工作

---
+++ CustomRP/Assets/LiteRP/Runtime/LiteRenderGraphRecorder.cs
@@ -0,0 +1,13 @@
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+
+namespace LiteRP
+{
+    public class LiteRenderGraphRecorder : IRenderGraphRecorder
+    {
+        public void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            
+        }
+    }
+}
