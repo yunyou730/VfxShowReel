a1e0f8d990d427d254b24e4f45d458559e95e54d
miaoyunlong <502765299@qq.com>
2025-02-27 21:49:33 +0800

    Compute Shader 里，做基础的数学计算

--- UnityVfxShowReel/Assets/_ComputeShaderTest1/CSTest.cs
+++ UnityVfxShowReel/Assets/_ComputeShaderTest1/CSTest.cs
@@ -1,28 +1,45 @@
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.Polybrush;
+using Debug = UnityEngine.Debug;
 
 
 namespace ayy
 {
     public class CSTest : MonoBehaviour
     {
+        // Generate render texture image
         public RenderTexture _rt = null;
         public ComputeShader _computeShader = null;
         public GameObject _showImage = null;
+        private int _width = 8;
+        private int _height = 8;
         
+        // Process image
         public Texture2D _originTexture = null;
         public ComputeShader _csImageProcess = null;
         
         
-        private int _width = 8;
-        private int _height = 8;
+        // Simple compute
+        public ComputeShader _csSimpleCompute = null;
+        private ComputeBuffer _inputBuffer = null;
+        private ComputeBuffer _outputBuffer = null;
+        private float[] _inputData;
+        private float[] _outputData;
+        private const int kDataSize = 65535;
+        
+        // stopwatch
+        Stopwatch stopwatch = new Stopwatch();
+        
 
         void Start()
         {
             _width = 16;
             _height = 16;
+
+            InitSimpleCompute();
         }
-
-
+        
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Q))
@@ -41,6 +58,14 @@ namespace ayy
             {
                 ProcessImageWithComputeShader();
             }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                ComputeWithCS();
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                ComputeWithCommon();
+            }            
         }
         
         private void CreateRenderTexture()
@@ -81,11 +106,55 @@ namespace ayy
             var material = _showImage.GetComponent<MeshRenderer>().material;
             material.SetTexture(Shader.PropertyToID("_MainTex"), rt);
             
-            //material.SetTexture(Shader.PropertyToID("_MainTex"), _originTexture);
             int kernel = _csImageProcess.FindKernel("CSMain");
             _csImageProcess.SetTexture(kernel,Shader.PropertyToID("InputTex"), _originTexture);
             _csImageProcess.SetTexture(kernel,Shader.PropertyToID("Result"), rt);
             _csImageProcess.Dispatch(kernel,width/8 ,height/8,1);
+        }
+
+
+        private void InitSimpleCompute()
+        {
+            _inputData = new float[kDataSize];
+            for (int i = 0;i < kDataSize;i++)
+            {
+                _inputData[i] = i;
+            }
+            
+            _inputBuffer = new ComputeBuffer(kDataSize, sizeof(float));
+            _inputBuffer.SetData(_inputData);
+
+            
+            _outputData = new float[kDataSize];
+            _outputBuffer = new ComputeBuffer(kDataSize, sizeof(float)); 
+        }
+
+        private void ComputeWithCS()
+        {
+            Debug.Log("ComputeWithCS"); 
+            stopwatch.Restart();
+            
+            int kernel = _csSimpleCompute.FindKernel("CSMain");
+            _csSimpleCompute.SetBuffer(kernel,Shader.PropertyToID("inputBuffer"), _inputBuffer);
+            _csSimpleCompute.SetBuffer(kernel,Shader.PropertyToID("outputBuffer"), _outputBuffer);
+            
+            int threadGroups = Mathf.CeilToInt((float)kDataSize / 64);;
+            _csSimpleCompute.Dispatch(kernel,threadGroups, 1, 1);
+            _outputBuffer.GetData(_outputData);
+
+            Debug.Log("done with cs, cost time:" + stopwatch.Elapsed);
+        }
+
+        private void ComputeWithCommon()
+        {
+            Debug.Log("ComputeWithCommon"); 
+            stopwatch.Restart();
+            
+            for (int i = 0;i < kDataSize;i++)
+            {
+                _outputData[i] = _inputData[i] * 2.0f;
+            }            
+            Debug.Log("done with common, cost time:" + stopwatch.Elapsed);
         }
     }
 }
