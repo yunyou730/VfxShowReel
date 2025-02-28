771b57f63e8a36c52bebaee528bb575dc974424d
miaoyunlong <502765299@qq.com>
2025-02-28 14:51:16 +0800

    增加 ComputeShader 控制粒子的代码 ，现在还不能 run 待再细看

--- UnityVfxShowReel/Assets/_ComputeShaderTest1/ManualParticleSystemByCS.cs
+++ UnityVfxShowReel/Assets/_ComputeShaderTest1/ManualParticleSystemByCS.cs
@@ -1,16 +1,36 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace ayy
 {
+    struct ParticleData
+    {
+        public Vector3 pos; // float3
+        public Color color; // float4
+    }
 
     public class ManualParticleSystemByCS : MonoBehaviour
     {
         public int _particleCount = 1000;
         private ParticleSystem _particleSystem;
         
+        private ComputeBuffer _buffer = null;
+
+        public ComputeShader _csParticle = null;
+        
         void Start()
         {
+            int size = Marshal.SizeOf<ParticleData>();
+            _buffer = new ComputeBuffer(_particleCount, size);
+            ParticleData[] data = new ParticleData[_particleCount];
+            _buffer.SetData(data);
+            
             _particleSystem = GetComponent<ParticleSystem>();
+
+            // var customData = _particleSystem.customData;
+            // customData.enabled = true;
+            // _particleSystem.main.simulationSpace = ParticleSystemSimulationSpace.World;
+            
         }
 
         void Update()
@@ -19,11 +39,22 @@ namespace ayy
             {
                 Emit();
             }
+            UpdateDataBufferWithCS();
         }
 
         private void Emit()
         {
             _particleSystem.Emit(_particleCount);
+        }
+
+        private void UpdateDataBufferWithCS()
+        {
+            int kernel = _csParticle.FindKernel("CSMain");
+            _csParticle.SetBuffer(kernel, Shader.PropertyToID("ParticlesBuffer"), _buffer);
+            _csParticle.SetFloat(Shader.PropertyToID("Time"), Time.time);
+            _csParticle.Dispatch(kernel, Mathf.CeilToInt(_particleCount/1000f),1, 1);
+            
+            
         }
     }
     
